d784f646f2762e8487694d1fb195ee95c08cae62
Raul Serrano Mena <30394974+raulserranomena@users.noreply.github.com>
2025-03-23 15:46:46 -0400

    Create neoWordLibData.js

---
+++ neoWordLibData.js
@@ -0,0 +1,10 @@
+// neoWordLibData.js
+
+var neoWordLibData = {
+  "Default Words": {
+    "Description": "STAY, AWAY, NIGHT, INSTANT, DELAY, etc.\n\n(Placeholder library.)"
+  },
+  "Additional Words": {
+    "Description": "FRONT DOOR, BACK DOOR, LIVING ROOM, etc.\n(Placeholder library.)"
+  }
+};
