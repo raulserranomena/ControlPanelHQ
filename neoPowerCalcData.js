61d5fa7a21ec724282694c4ea309264a6fa658e1
Raul Serrano Mena <30394974+raulserranomena@users.noreply.github.com>
2025-03-23 15:47:13 -0400

    Create neoPowerCalcData.js

---
+++ neoPowerCalcData.js
@@ -0,0 +1,7 @@
+// neoPowerCalcData.js
+
+var neoPowerCalcData = {
+  "Overview": {
+    "Description": "Enter device current draws to estimate total system consumption.\n\n(Placeholder. Possibly create a UI form in HTML if needed.)"
+  }
+};
