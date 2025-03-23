6c2d20875605c6bbc28c00479c835430e7f1929b
Raul Serrano Mena <30394974+raulserranomena@users.noreply.github.com>
2025-03-23 15:47:42 -0400

    Create neoDocData.js

---
+++ neoDocData.js
@@ -0,0 +1,10 @@
+// neoDocData.js
+
+var neoDocData = {
+  "Manuals": {
+    "Description": "Link to official DSC NEO Installation Manual, User Guide, etc.\n\n(Placeholder links or references.)"
+  },
+  "Additional Resources": {
+    "Description": "Any extra docs, PDF downloads, FAQ pages. (Placeholder.)"
+  }
+};
