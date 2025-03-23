66301926495ea6c53fdc923e4b914352ff787bf7
Raul Serrano Mena <30394974+raulserranomena@users.noreply.github.com>
2025-03-23 15:46:11 -0400

    Create neoReportData.js

---
+++ neoReportData.js
@@ -0,0 +1,10 @@
+// neoReportData.js
+
+var neoReportData = {
+  "Contact ID Codes": {
+    "Description": "Typical CID codes for burglary, fire, panic, etc.\n\n(Placeholder data.)"
+  },
+  "SIA Codes": {
+    "Description": "Sample SIA event codes. (Placeholder data.)"
+  }
+};
