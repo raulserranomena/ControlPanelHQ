8c8f3ac81ef39d137074ba07fc0e4aed14e9a440
Raul Serrano Mena <30394974+raulserranomena@users.noreply.github.com>
2025-03-23 15:45:31 -0400

    Create neoTroubleData.js

---
+++ neoTroubleData.js
@@ -0,0 +1,10 @@
+// neoTroubleData.js
+
+var neoTroubleData = {
+  "General Issues": {
+    "Description": "AC Fail, Battery Low, Telephone Line Trouble.\n\nAdd real troubleshooting steps here..."
+  },
+  "False Alarms": {
+    "Description": "Causes: Loose wiring, faulty sensors, user error, etc.\n\n(Placeholder.)"
+  }
+};
